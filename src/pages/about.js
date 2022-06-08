bd229b7beaaadb0a386fd19e2a0dc407dc068363
CaityJ98 <caitlynjschaper@gmail.com>
2022-06-08 18:37:17 +1000

    Added dynamic and responsive homepage using CSS styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,16 @@
-import Logo from '../assets/images/cslogo.png';
+
 import '../App.scss'
 
 function About () {
     return (
         <div className="page">
             <div className="wrapper">
-                <img src={Logo} alt="CS Logo"/>
+              
                 <h1 className="header">About Me</h1>
-                <p> I'm a full stack developer </p>
+                <div className="blurb">
+                    <p> I'm a full stack developer </p>
+                    </div>
+                
             </div>
 
         </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@
 // import App from '../App.js';
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Picture from '../assets/images/background.jpg'
 
 
 function Home () {
@@ -9,12 +10,20 @@ function Home () {
     return(
        
         <div className="page">
-            
-           <div class="picture">
+            <img src={Picture} alt='background img'/>
+           
+                <h1 className="header"> Caitlyn Schaper</h1>
+                <div className="hometext">
+                <p>&lt;Full Stack Developer in Training&gt;</p>
+                <div className="homeblurb">   
+                    <p>Hi! I'm Caitlyn, a soon to be graduate of Coder Academy. If you'd like to find out more about me
+                    or view some of my projects please visit my &lt;About Me&gt; page and if you'd like to get in contact
+                    please visit the &lt;Contact Page&gt;. I look forward to hearing from you!!
+                </p>
+                </div>
+             
+                </div>
                 
-                </div> 
-                <h1 className="header"> Hi my name is Caitlyn!</h1>
-                <p>I'm a full stack developer.</p>
           
             
        
